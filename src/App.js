d34bb87c63381968db3a5b2700bc16fa5f6444b2
castro-d <castro.dh91@gmail.com>
2022-11-25 20:49:16 -0300

    implement search anime

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import jikan from "./service/jikan";
+
+const Anime = (props) => {
+  return (
+    <div>
+      <p>{props.title}</p>
+      <p>{props.duration}</p>
+    </div>
+  )
+}
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [result, setResult] = useState('');
+  
+  const handleSubmit = async (term) => {
+    const data = await jikan.searchAnime(term)
+    setResult(data)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MyAnime app</h1>
+      <h2>Search for an anime</h2>
+      <input onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} placeholder="Naruto.." />
+      <button onClick={() => handleSubmit(searchTerm)}>Go</button>
+      {result && (
+        result.data.map((a) => (
+          <Anime key={a.mal_id} title={a.title} duration={a.duration} />
+        ))
+      )}
     </div>
   );
 }
